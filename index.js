ca36b3fb0e120f9b11ebf92dba5ea6abc357a594
Niortien <niortiencoulibaly2001@gmail.com>
2023-07-28 09:11:46 +0000

    HOME_v2

---
+++ index.js
@@ -0,0 +1,19 @@
+let iconClose=document.querySelector(".close_img")
+let inconMenu=document.querySelector(".menu_img")
+
+
+iconClose.addEventListener("click",closeMenu)
+inconMenu.addEventListener("click",showMenu)
+
+function closeMenu(){
+   let folding_menu=document.querySelector(".folding_menu")
+   folding_menu.classList.remove(".show_menu")
+  
+}
+function showMenu(){
+   let folding_menu=document.querySelector(".folding_menu")
+   folding_menu.classList.add(".show_menu")
+   alert("issa")
+
+
+}
